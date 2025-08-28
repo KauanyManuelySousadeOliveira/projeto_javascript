424c91b051705638f9119c32dce77b41b108a7eb
KauanyManuelySousadeOliveira <00001119772886sp@al.educacao.sp.gov.br>
2025-08-28 14:27:26 -0300

    criamos funçoõ

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,31 @@ const modalRegister = document.querySelector("#modal-register");
 const btnCancelLogin = document.querySelector("#btn-cancel-login");
 const btnCancelRegister = document.querySelector("#btn-cancel-register");
 
+function soma(num01, num02) {
+  const somar = num01 + num02;
+  console.log(somar);
+}
+
+soma(8, 6)
+
+// arrow function 
+const multiplicar = (num01, num02) => {
+  return num01 * num02
+}
+
+console.log(multiplicar(5, 8.9));
+
+// arrow function 
+const subtrair = (num01, num02) => {
+  return num01 - num02
+}
+
+console.log(subtrair(8, 5));
+
+
+// addEventListener => escuta o evento de click
+// e executa a funcao
+
 btnLogin.addEventListener("click", (e) => {
   modalLogin.classList.remove("hide-modal");
   modalLogin.classList.add("show-modal");
