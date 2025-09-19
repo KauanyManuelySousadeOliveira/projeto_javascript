a199a89adb68d9d5c13f900e693ae975c6237930
KauanyManuelySousadeOliveira <00001119772886sp@al.educacao.sp.gov.br>
2025-09-19 12:04:11 -0300

    criando funçoes func

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -11,11 +11,26 @@ function mensagemError() {
 
 mensagemError();
 
-// Função simples sem parâmetro
+// Função simples com parâmetro
 function receberParametro(recebendoTexto) {
     console.log(RecebendoTexto);
 }
 
 receberParametro("Enviando texto para a função!");
 
+function receberNumero(recebendoNumero) {
+    console.log(recebendoNumero);
+}
+
+receberNumero(5);
+
+function funcionarios(nome,email,salario) {
+    const func = `
+    o nome do funcionario é ${nome}, o email é ${email}, o seu salario é ${salario}
+    `
+    console.log(func);
+}
+
+
+funcionarios("Kauany", "kauany@kakau.com", 5000)
 
